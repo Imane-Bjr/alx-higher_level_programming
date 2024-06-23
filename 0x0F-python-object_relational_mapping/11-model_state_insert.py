db85d3d983ace21f530fb1edcb4062eaca93ed39
Imane-Bjr <boujirimane1@gmail.com>
2024-06-23 14:44:39 -0700

    Task 6, 7, 8, 9, 10, 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
+"""Fetch All the Data From the table states"""
 import sys
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-from sqlalchemy import create_engine, select, insert
+
 
 if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
-    with engine.connect() as connection:
-        transaction = connection.begin()
-        query = insert(State).values(name="Louisiana")
-        connection.execute(query)
-        query = select(State).where(State.name == "Louisiana")
-        state = connection.execute(query).fetchone()
-        if state:
-            print(state.id)
-        transaction.commit()
-
-    engine.dispose()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    sentence = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(sentence.format(
+        username, password, database), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # state = session.query(State.id).where(State.name == name_state).first()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    # Commit the session to persist the changes
+    session.commit()
+    # print(state) -> None so the add didn't return anything
+    # state = session.query(State.id).where(State.name == 'Louisiana').first()
+    state = session.query(State.id).where(State.name == new_state.name).first()
+    print(state.id)
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
+"""Fetch All the Data From the table states"""
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
 
 if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
-    with engine.connect() as connection:
-        query = select(State).order_by(State.id.asc()) \
-            .where(State.name.like("%a%"))
-        states = connection.execute(query)
-        for state in states:
-            print(f"{state.id}: {state.name}")
-
-    engine.dispose()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    sentence = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(sentence.format(
+        username, password, database), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).\
+        filter(State.name.like(f'%a%')).order_by(State.id.asc()).all()
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
